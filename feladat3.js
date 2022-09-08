2bdc595cd9b9d90b317d30347846076d8676b991
avpboy <somogyibotond03@gmail.com>
2022-09-08 13:06:25 +0200

    setinterval-lal

--- feladat3.js
+++ feladat3.js
@@ -1,10 +1,7 @@
 
-delay()
 let count = 0
-console.log(count);
-async function delay() {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    count++
-    console.log(count);
-    delay()
-  }
+
+setInterval(function(){
+  console.log(count)
+  count++
+},2000)
